312c20027993765e09af56a45954c4c8f6b68bdc
Meredith Lampe <meredithlampe@Merediths-MacBook-Pro.local>
2022-05-05 16:08:50 -0400

    add funny windows box

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 $(function () {
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    console.log(vw);
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
     let speed = 50000;
     if (vw < 768) {
         speed = 5000000;
@@ -23,27 +23,27 @@ $(function () {
     var anim1 = new ASCIIAnimation(["1","2","3"], 100, div1);
     */
 
-    function ASCIIAnimation(animArray, speed, DOMtarget) {
-      var currentFrame = 0;
-        for(var i = 0; i < animArray.length; i++) {
-            animArray[i] = animArray[i].replace(/ /g,"&nbsp;");
-            animArray[i] = "<pre>" + animArray[i] + "</pre>";
-        }
-        DOMtarget.innerHTML = animArray[0];
-        currentFrame++;
-        this.animation = setInterval(function() {
-            DOMtarget.innerHTML = animArray[currentFrame];
-            currentFrame++;
-            if(currentFrame >= animArray.length) currentFrame = 0;
-        }, speed);
-        this.getCurrentFrame = function() {
-            return currentFrame;
-        }
-    }
+    // function ASCIIAnimation(animArray, speed, DOMtarget) {
+    //   var currentFrame = 0;
+    //     for(var i = 0; i < animArray.length; i++) {
+    //         animArray[i] = animArray[i].replace(/ /g,"&nbsp;");
+    //         animArray[i] = "<pre>" + animArray[i] + "</pre>";
+    //     }
+    //     DOMtarget.innerHTML = animArray[0];
+    //     currentFrame++;
+    //     this.animation = setInterval(function() {
+    //         DOMtarget.innerHTML = animArray[currentFrame];
+    //         currentFrame++;
+    //         if(currentFrame >= animArray.length) currentFrame = 0;
+    //     }, speed);
+    //     this.getCurrentFrame = function() {
+    //         return currentFrame;
+    //     }
+    // }
 
-    ASCIIAnimation.prototype.stopAnimation = function() {
-        clearInterval(this.animation);
-    }
+    // ASCIIAnimation.prototype.stopAnimation = function() {
+    //     clearInterval(this.animation);
+    // }
 
     var div3 = document.createElement('div');
     let asciiContainer = $('.silly-ascii-thing');
